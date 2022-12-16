bb78f53bff2a108bf92094fd7a383edca2ecfc1a
Oleg Osenov <olejka_oon@mail.ru>
2022-12-16 21:52:56 +0500

    переменые в методе

---
+++ src/main/java/testMaikMakGrat/Scope.java
@@ -0,0 +1,15 @@
+package testMaikMakGrat;
+
+public class Scope {
+    public static void main(String[] args) {
+
+        String txt = "Это локальная переменная метода main" ;
+        System.out.println(txt);
+        sub();
+    }
+
+    public static void sub(){
+        String txt = "Это локальная переменная метода sub()";
+        System.out.println(txt);
+    }
+}
