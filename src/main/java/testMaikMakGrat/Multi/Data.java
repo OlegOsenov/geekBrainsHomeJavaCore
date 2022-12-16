f9ebdf0d75cc277f25cacfc0b0a9ff97e10a804e
Oleg Osenov <olejka_oon@mail.ru>
2022-12-16 23:18:11 +0500

    Добавил класс Draw

--- src/main/java/testMaikMakGrat/Multi/Data.java
+++ src/main/java/testMaikMakGrat/Multi/Data.java
@@ -1,4 +1,9 @@
 package testMaikMakGrat.Multi;
 
 public class Data {
+    public final static String txt = "Это глобальная переменная класса Data";
+
+    public static void greeting(){
+        System.out.println("Это глобальный метод класса Data");
+    }
 }
