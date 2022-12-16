1dd4ca04728d426becbd90fd1c75243a7a67732a
Oleg Osenov <olejka_oon@mail.ru>
2022-12-16 22:05:59 +0500

    final глобальная переменная

--- src/main/java/testMaikMakGrat/Scope.java
+++ src/main/java/testMaikMakGrat/Scope.java
@@ -1,10 +1,12 @@
 package testMaikMakGrat;
 
 public class Scope {
+    final static String txt = "Это локальная переменная метода Scope" ;
+
     public static void main(String[] args) {
 
         String txt = "Это локальная переменная метода main" ;
-        System.out.println(txt);
+        System.out.println(Scope.txt);
         sub();
     }
 
