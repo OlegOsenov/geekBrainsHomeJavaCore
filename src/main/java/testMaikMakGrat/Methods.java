66933712390644aed81c019b6e25581074875c73
Oleg Osenov <olejka_oon@mail.ru>
2022-12-16 21:40:13 +0500

    создание методов

---
+++ src/main/java/testMaikMakGrat/Methods.java
@@ -0,0 +1,13 @@
+package testMaikMakGrat;
+
+public class Methods {
+    public static void main(String[] args) {
+        System.out.println("Сообщение из главного метода main.");
+
+        sub();
+    }
+
+    public static void sub(){
+        System.out.println("Сообщение из метода sub().");
+    }
+}
