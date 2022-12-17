25aed00f1f2f48e92d5c4355473d52a7a99b7282
Oleg Osenov <olejka_oon@mail.ru>
2022-12-17 19:43:26 +0500

    обработал метод echo() ,блоком try catch

--- src/main/java/testMaikMakGrat/SuperClass/SubClass.java
+++ src/main/java/testMaikMakGrat/SuperClass/SubClass.java
@@ -1,4 +1,18 @@
 package testMaikMakGrat.SuperClass;
 
 public class SubClass extends SuperClass{
+    public static void main(String[] args) {
+
+        hello();
+        SuperClass.hello();
+        try {
+            echo(args[0]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Ввода данных не было");
+        }
+
+    }
+    public static void hello(){
+        System.out.println("Привет из Подкласса");
+    }
 }
