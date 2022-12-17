aa27bf6ad4b78b33a416bc32df454a5457d7adaf
Oleg Osenov <olejka_oon@mail.ru>
2022-12-17 21:24:16 +0500

    Добавил глобальные переменные и метод accelerate()

--- src/main/java/testMaikMakGrat/Car/Car.java
+++ src/main/java/testMaikMakGrat/Car/Car.java
@@ -1,4 +1,12 @@
 package testMaikMakGrat.Car;
 
 public class Car {
+
+    public final static String color = "Красный";
+    public final static String bodyType = "Купе";
+
+    public static String accelerate(){
+        String motion = "Ускоряется...";
+        return motion;
+    }
 }
