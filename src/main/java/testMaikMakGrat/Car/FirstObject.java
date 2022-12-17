c70713a1e25a013bea94e81e9ba9592da694f225
Oleg Osenov <olejka_oon@mail.ru>
2022-12-17 21:27:30 +0500

    Создан класс FirstObject и созданы обьекты

---
+++ src/main/java/testMaikMakGrat/Car/FirstObject.java
@@ -0,0 +1,10 @@
+package testMaikMakGrat.Car;
+
+public class FirstObject {
+    public static void main(String[] args) {
+
+        System.out.println("Color " + Car.color);
+        System.out.println("Body type " + Car.bodyType);
+        System.out.println(Car.accelerate());
+    }
+}
