ee3be6f8e52a16ee490810cff5df49df7c45a8d7
Oleg Osenov <olejka_oon@mail.ru>
2022-12-17 21:43:57 +0500

    Car version 2

---
+++ src/main/java/testMaikMakGrat/CarVersion2/Car.java
@@ -0,0 +1,11 @@
+package testMaikMakGrat.CarVersion2;
+
+public class Car {
+    public final static String color = "Красный";
+    public final static String bodyType = "Купе";
+
+    public static String accelerate(){
+        String motion = "Ускоряется...";
+        return motion;
+    }
+}
---
+++ src/main/java/testMaikMakGrat/CarVersion2/FirstInstance.java
@@ -0,0 +1,14 @@
+package testMaikMakGrat.CarVersion2;
+
+public class FirstInstance {
+    public static void main(String[] args) {
+
+        System.out.println("Цвет автомобиля " + Car.color);
+        System.out.println("Тип кузова " + Car.bodyType);
+        System.out.println(Car.accelerate());
+
+        Car Porsche = new Car();
+        System.out.println("Цвет Porsche " + Porsche.color);
+        System.out.println("Тип кузова Porsche " + Porsche.bodyType);
+    }
+}
