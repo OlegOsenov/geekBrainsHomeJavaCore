53d06d98368a727f86dac78cfe25937edbb155a8
Oleg Osenov <olejka_oon@mail.ru>
2022-12-18 21:51:42 +0500

    Car version 3, добавил методы accelerate()

--- src/main/java/testMaikMakGrat/CarVersion3/Car.java
+++ src/main/java/testMaikMakGrat/CarVersion3/Car.java
@@ -4,4 +4,20 @@ public class Car {
     private String maker;
     private String color;
     private String bodyType;
+
+    private String accelerate(){
+        String motion = "Ускоряется...";
+        return motion;
+    }
+    public void setCar(String brand, String paint, String style){
+        maker = brand;
+        color = paint;
+        bodyType = style;
+    }
+
+    {
+        System.out.println(maker + " цвет " + color);
+        System.out.println(maker + " тип кузова " + bodyType);
+        System.out.println(maker + "  " + accelerate() + "\n");
+    }
 }
