cdb83ebdfd3c8beadf358a1c8328de2d7e232109
Oleg Osenov <olejka_oon@mail.ru>
2022-12-19 22:51:02 +0500

    Чтение данных

---
+++ src/main/java/testMaikMakGrat/ReadFile.java
@@ -0,0 +1,28 @@
+package testMaikMakGrat;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+
+        try {
+            FileReader file = new FileReader("oscar1.txt");
+            BufferedReader buffer = new BufferedReader(file);
+            String line = "";
+            while ((line = buffer.readLine()) != null){
+                System.out.println(line);
+                buffer.close();
+            }
+
+
+        }
+        catch (IOException e){
+            System.out.println("Произошла ошибка");
+        }
+
+
+
+    }
+}
