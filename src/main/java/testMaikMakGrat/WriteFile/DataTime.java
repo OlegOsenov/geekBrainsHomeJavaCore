ab48ba72bac678fc63545b2ba71f2ceb26d5d48b
Oleg Osenov <olejka_oon@mail.ru>
2022-12-25 20:15:57 +0500

    Вывод на консоль отдельно даты и времени

--- src/main/java/testMaikMakGrat/WriteFile/DataTime.java
+++ src/main/java/testMaikMakGrat/WriteFile/DataTime.java
@@ -8,5 +8,18 @@ public class DataTime {
         LocalDateTime date = LocalDateTime.now();
         System.out.println("\nСейчас " + date);
 
+        date = date.withYear(2024);
+        System.out.println("\nТеперь текущее время " + date);
+        String fields = "\nГод:\t\t\t" + date.getYear();
+        fields += "\nМесяц:\t\t\t" + date.getMonth();
+        fields += "\nНомер Месяца:\t\t\t" + date.getMonthValue();
+        fields += "\nДень недели:\t\t" + date.getDayOfWeek();
+        fields += "\nДень месяца:\t\t" + date.getDayOfMonth();
+        fields += "\nДень в году:\t\t" + date.getDayOfYear();
+        fields += "\nЧас (0-23):\t\t" + date.getHour();
+        fields += "\nМинута:\t\t" + date.getMinute();
+        fields += "\nСекунда:\t\t" + date.getSecond();
+        System.out.println(fields);
+
     }
 }
