c12fcb9e3d25cc4a366b521bd89e5b3abe626cc3
Oleg Osenov <olejka_oon@mail.ru>
2022-12-25 19:41:06 +0500

    Создал список

---
+++ src/main/java/testMaikMakGrat/List.java
@@ -0,0 +1,15 @@
+package testMaikMakGrat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class List {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Alfa");
+        list.add("Delta");
+        list.add("Charli");
+        list.add("Beta");
+        System.out.println("Список " + list);
+    }
+}
