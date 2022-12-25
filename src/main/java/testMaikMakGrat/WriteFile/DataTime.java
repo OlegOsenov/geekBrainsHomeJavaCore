bcab09538acb474e3bcf7d52be8de0c0232e075f
Oleg Osenov <olejka_oon@mail.ru>
2022-12-25 20:05:32 +0500

    Вывод на консоль текущей даты

--- src/main/java/testMaikMakGrat/WriteFile/DataTime.java
+++ src/main/java/testMaikMakGrat/WriteFile/DataTime.java
@@ -1,7 +1,12 @@
 package testMaikMakGrat.WriteFile;
 
+import java.time.LocalDateTime;
+
 public class DataTime {
     public static void main(String[] args) {
+
+        LocalDateTime date = LocalDateTime.now();
+        System.out.println("\nСейчас " + date);
 
     }
 }
