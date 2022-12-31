58242184c53feb7b475525d94828fb79f75bb4e5
Oleg Osenov <olejka_oon@mail.ru>
2022-12-31 16:06:52 +0500

    Работа с числами. Сделал под каждый метод одно действие

--- src/main/java/testEasyCodeRu/ShowSum/Main.java
+++ src/main/java/testEasyCodeRu/ShowSum/Main.java
@@ -2,14 +2,28 @@ package testEasyCodeRu.ShowSum;
 
 public class Main {
     public static void main(String[] args) {
-        showSum("3", "4");
+        print(multiply(sum(3, 4), difference(7, 5)));
+        print(multiply(sum(40,60),difference(300,270)));
+        print(multiply(sumString("14","14"),difference(100,200)));
     }
 
-    private static void showSum(Integer numb1, Integer numb2) {
-        System.out.println(numb1 + numb2);
+    private static int sum(int numb1, int numb2) {
+        return numb1 + numb2;
     }
 
-    private static void showSum(String numb1, String numb2) {
-        System.out.println(Integer.valueOf(numb1) + Integer.valueOf(numb2));
+    private static int sumString(String numb1, String numb2) {
+        return (Integer.valueOf(numb1) + Integer.valueOf(numb2));
+    }
+
+    private static void print(int numb) {
+        System.out.println(numb);
+    }
+
+    private static int difference(int numb1, int numb2) {
+        return numb1 - numb2;
+    }
+
+    private static int multiply(int numb1, int numb2) {
+        return numb1 * numb2;
     }
 }
