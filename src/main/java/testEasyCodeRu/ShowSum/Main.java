c638ad6fdb8fc8c487e30e8ea683e285ba05ed55
Oleg Osenov <olejka_oon@mail.ru>
2022-12-31 16:22:49 +0500

    пременные, создание и переименование

--- src/main/java/testEasyCodeRu/ShowSum/Main.java
+++ src/main/java/testEasyCodeRu/ShowSum/Main.java
@@ -2,7 +2,10 @@ package testEasyCodeRu.ShowSum;
 
 public class Main {
     public static void main(String[] args) {
-        print(multiply(sum(3, 4), difference(7, 5)));
+        int sum1 = sum(3, 4);
+        int diff1 = difference(7, 5);
+        int multiplication1 = multiply(sum1, diff1);
+        print(multiplication1);
         print(multiply(sum(40,60),difference(300,270)));
         print(multiply(sumString("14","14"),difference(100,200)));
     }
