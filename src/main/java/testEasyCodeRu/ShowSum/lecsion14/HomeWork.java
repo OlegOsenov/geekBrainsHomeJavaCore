5ae371e00d7454da85290b33e7e88d561df85003
Oleg Osenov <olejka_oon@mail.ru>
2023-01-02 12:56:16 +0500

    Лекция 14 домашняя работа, №2

--- src/main/java/testEasyCodeRu/ShowSum/lecsion14/HomeWork.java
+++ src/main/java/testEasyCodeRu/ShowSum/lecsion14/HomeWork.java
@@ -2,12 +2,30 @@ package testEasyCodeRu.ShowSum.lecsion14;
 
 public class HomeWork {
     public static void main(String[] args) {
-        print("Задание 1 :");
+        print("Задание 1:");
         multipleOfThree(new int[]{1, 2, 3, 4, 5, 6, 7, -7});
-        multipleOfThree(new int[]{});
         multipleOfThree(new int[]{3, 6, 9, 12, 13, 15});
-        multipleOfThree(new int[]{1,2,4,5});
+        multipleOfThree(new int[]{1, 2, 4, 5});
+        multipleOfThree(new int[]{});
+        print("--------------------------------------------");
+        print("Задание 2:");
+        checkMultiple(2, new int[]{1, 2, 3, 4});
+        checkMultiple(6, new int[]{9, 2, 13, 24});
+        print("--------------------------------------------");
+        print("Задание 3:");
 
+    }
+
+    private static void checkMultiple(int check, int[] array) {
+        int temp = 0;
+        isEmptyArray(array);
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % check == 0) {
+                temp = array[i];
+                printF("[" + temp + "]");
+            }
+        }
+        print("");
     }
 
     private static void multipleOfThree(int[] array) {
